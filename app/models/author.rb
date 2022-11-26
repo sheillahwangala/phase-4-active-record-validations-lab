8b69f55cdd58cc8e85a35034d4ad801c7040f835
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-26 13:48:29 +0300

    completed assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ApplicationRecord
+    # validates authors have a name, name cannot be emppty and is unique
+    validates :name, presence: true, uniqueness: true
+
+    # validates authors' phone numbers are exactly 10 digits
+    validates :phone_number, length: { is:10 }
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+
+    # validates title cannot be empty (posts have title)
+    validates :title, presence: true
+
+    # post content has at least 250 characters
+    validates :content, length: { minimum:250 }
+
+    # post summary has at most 250 characters
+    validates :summary, length: { maximum:250 }
+
+    # post category is either 'Fiction' or 'Non-Fiction'. This requires inclusion validator, refer to the Rails guide Links to an external site
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    # custom validations
+    validate :clickbait?
+
+    def clickbait?
+        if title && !title.match(/Won't Believe |secret| Top \d|Guess/)
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
